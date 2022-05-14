270c67180869be5310dfe41cacee308b2c69b66e
matt-carmo <matheus_2018@hotmail.com>
2022-05-14 13:06:06 -0300

    enu-burguer-finish

--- js/menu-burguer.js
+++ js/menu-burguer.js
@@ -1,6 +1,24 @@
-const menuBurguerIcon = document.querySelector('.fa-bars')
+// const menuBurguerIcon = document.querySelector('.fa-bars')
 const menuBurguerDiv = document.querySelector('.menu-burguer')
-menuBurguerIcon.addEventListener('click', () => {
+// menuBurguerIcon.addEventListener('click', () => {
+const pfvFunciona = document.querySelector('.section')
+// })
+
+const nav1Icon = document.querySelector('#nav-icon1')
+nav1Icon.addEventListener('click', () => {
+    nav1Icon.classList.toggle('open')
     menuBurguerDiv.classList.toggle('active')
-    menuBurguerIcon.classList.toggle('active')
 })
+ pfvFunciona.addEventListener('click', (e) => {
+    if (e.target !==  nav1Icon) {
+        menuBurguerDiv.classList.remove('active')
+        nav1Icon.classList.remove('open')
+    }})
+
+
+
+// $(document).ready(function(){
+// 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
