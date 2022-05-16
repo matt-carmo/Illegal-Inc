66f21fcd78005bc98e0169c734035dac0e036b08
ERICH HONICKER RINALDI VALARINI <erich.hrvalarini@senacsp.edu.br>
2022-05-16 19:45:28 -0300

    Slide

--- slideShow.js
+++ slideShow.js
@@ -1,12 +1,25 @@
-var splide = new Splide( '.splide', {
-    perPage : 4,
-    cover   : true,
-    height  : '30rem',
+var splide = new Splide('.splide', {
+  perPage: 4,
+  cover: true,
+  height: '30rem',
+  lazyLoad: 'nearby',
+
+});
+
+if (window.matchMedia("(max-width: 450px)").matches) {
+  var splide = new Splide('.splide', {
+    perPage: 1,
+    cover: true,
+    height: '30rem',
     lazyLoad: 'nearby',
-  
-  } );
-  
-  
-  
-  
+    
+  });
   splide.mount();
+  
+} 
+else {
+
+}
+
+
+splide.mount();
