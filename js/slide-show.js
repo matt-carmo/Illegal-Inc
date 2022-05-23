44d1f1eee9fde7dad50219158eca7564e63f68c3
matt-carmo <matheus_2018@hotmail.com>
2022-05-23 15:03:13 -0300

    js/slide-show

---
+++ js/slide-show.js
@@ -0,0 +1,25 @@
+var splide = new Splide('.splide', {
+  perPage: 4,
+  cover: true,
+  height: '40rem',
+  lazyLoad: 'nearby',
+
+});
+
+if (window.matchMedia("(max-width: 600px)").matches) {
+  var splide = new Splide('.splide', {
+    perPage: 1,
+    cover: true,
+    height: '30rem',
+    lazyLoad: 'nearby',
+    
+  });
+  splide.mount();
+  
+} 
+else {
+
+}
+
+
+splide.mount();
