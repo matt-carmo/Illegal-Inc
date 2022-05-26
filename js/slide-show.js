409cc42b9e867dc33a8545b3af12b4069f9a247e
MATHEUS HENRIQUE GONZAGA DO CARMO <matheus.hgcarmo@senacsp.edu.br>
2022-05-25 22:27:12 -0300

    fizemos-no-senac

--- js/slide-show.js
+++ js/slide-show.js
@@ -1,25 +1,57 @@
-var splide = new Splide('.splide', {
-  perPage: 4,
-  cover: true,
-  height: '40rem',
-  lazyLoad: 'nearby',
 
-});
+
+const splider = document.querySelectorAll('.splide')
+
+splider.forEach(element => {
+  var splide = new Splide(element, {
+    perPage: 3,
+    cover: true,
+    height: '40rem',
+    lazyLoad: 'nearby',
+    autoScroll:true,
+    
+    type   : 'loop',
+    drag   : 'free',
+    focus  : 'center',
+    autoScroll: {
+      speed: 1,
+    },  
+    
+  
+  });
+  splide.mount();
+
+  
 
 if (window.matchMedia("(max-width: 600px)").matches) {
-  var splide = new Splide('.splide', {
+  splide.destroy()
+ 
+  var splide1 = new Splide(element, {
     perPage: 1,
     cover: true,
     height: '30rem',
     lazyLoad: 'nearby',
+
+    
+
     
   });
-  splide.mount();
+  splide1.mount();
   
 } 
 else {
 
 }
 
+  
+  
 
-splide.mount();
+});
+
+
+
+
+
+
+
+
