4e2d4f59184274fb0d54a1edbe985b8b17c15926
matt-carmo <matheus_2018@hotmail.com>
2022-05-27 13:41:45 -0300

    responsive-slide-showv1.0

--- js/slide-show.js
+++ js/slide-show.js
@@ -4,7 +4,10 @@ const splider = document.querySelectorAll('.splide')
 
 splider.forEach(element => {
   var splide = new Splide(element, {
-    perPage: 3,    
+    perPage: 3,
+    cover: true,
+    height: '40rem',
+    
     lazyLoad: 'nearby',
     autoScroll:true,
     
@@ -21,25 +24,21 @@ splider.forEach(element => {
 
   
 
-if (window.matchMedia("(max-width: 600px)").matches) {
-  splide.destroy()
+// if (window.matchMedia("(max-width: 600px)").matches) {
+//   splide.destroy()
  
-  var splide1 = new Splide(element, {
-    perPage: 1,
-    cover: true,
-    height: '30rem',
-    lazyLoad: 'nearby',
-
-    
-
-    
-  });
-  splide1.mount();
+//   var splide1 = new Splide(element, {
+//     perPage: 1,
+//     cover: true,
+//     height: '30rem',
+//     lazyLoad: 'nearby',    
+//   });
+//   splide1.mount();
   
-} 
-else {
+// } 
+// else {
 
-}
+// }
 
   
   
