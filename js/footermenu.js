cb9abb743a31f2131e3cf273027a69fc5353e6ec
Erich <erich.hrvalarini@senacsp.edu.br>
2022-06-09 21:05:08 -0300

    footerfeito

--- js/footermenu.js
+++ js/footermenu.js
@@ -1,11 +1,12 @@
-const listFooter = document.querySelector('.list-footer');
 const btnFooter = document.querySelector('.btn-footer');
+const listFooter = document.querySelector(".list-footer");
 
-btnFooter.addEventListener('click', ()=>{
- 
-})
+btnFooter.addEventListener("click", function() {
+   listFooter.classList.toggle('active2');
 
-
+    
+  
+});
 
 
 
